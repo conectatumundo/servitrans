557158ed96b855fbd053d257923910853512ccc5
victor alfonso <alfonso.hugo@hotmail.com>
2018-04-03 23:38:59 -0500

    add icon

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -28,72 +28,47 @@ $this->title = Yii::$app->id;
             <div class="row">
                 <!-- Item -->
                 <div class="one-third wow fadeIn">
-                    <span class="circle"><span class="icon  icon-themeenergy_savings"></span></span>
-                    <h3>Fixed rates</h3>
+                    <span class="circle"><span class="icon  icon-themeenergy_car-1"></span></span>
+                    <h3>Taxi</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
                 </div>
                 <!-- //Item -->
                 
                 <!-- Item -->
                 <div class="one-third wow fadeIn" data-wow-delay=".2s">
-                    <span class="circle"><span class="icon icon-themeenergy_lockpad"></span></span>
-                    <h3>Reliable transfers</h3>
+                    <span class="circle"><span class="icon icon-themeenergy_car-2"></span></span>
+                    <h3>Automóviles</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
                 </div>
                 <!-- //Item -->
                 
                 <!-- Item -->
                 <div class="one-third wow fadeIn" data-wow-delay=".4s">
-                    <span class="circle"><span class="icon icon-themeenergy_open-wallet"></span></span>
-                    <h3>No booking fees</h3>
+                    <span class="circle"><span class="icon icon-themeenergy_car-2"></span></span>
+                    <h3>Camionetas</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
                 </div>
                 <!-- //Item -->
                 
                 <!-- Item -->
                 <div class="one-third wow fadeIn">
-                    <span class="circle"><span class="icon icon-themeenergy_heart"></span></span>
-                    <h3>Free cancellation</h3>
+                    <span class="circle"><span class="icon  icon-themeenergy_bus"></span></span>
+                    <h3>Buses</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
                 </div>
                 <!-- //Item -->
                 
                 <!-- Item -->
                 <div class="one-third wow fadeIn" data-wow-delay=".2s">
-                    <span class="circle"><span class="icon icon-themeenergy_magic-trick"></span></span>
-                    <h3>Booking flexibility</h3>
+                    <span class="circle"><span class="icon icon-themeenergy_bus""></span></span>
+                    <h3>Busetas</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
                 </div>
                 <!-- //Item -->
-                
-                <!-- Item -->
-                <div class="one-third wow fadeIn" data-wow-delay=".4s">
-                    <span class="circle"><span class="icon icon-themeenergy_call"></span></span>
-                    <h3>24h customer service</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
-                </div>
-                <!-- //Item -->
-                
-                <!-- Item -->
-                <div class="one-third wow fadeIn">
-                    <span class="circle"><span class="icon icon-themeenergy_cup"></span></span>
-                    <h3>Award winning service</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
-                </div>
-                <!-- //Item -->
-                
                 <!-- Item -->
                 <div class="one-third wow fadeIn" data-wow-delay=".2s">
-                    <span class="circle"><span class="icon icon-themeenergy_attach"></span></span>
-                    <h3>Benefits for partners</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
-                </div>
-                <!-- //Item -->
-                
-                <!-- Item -->
-                <div class="one-third wow fadeIn" data-wow-delay=".4s">
-                    <span class="circle"><span class="icon icon-themeenergy_stars"></span></span>
-                    <h3>Quality vehicles</h3>
+                    <span class="circle"><span class="icon icon-themeenergy_bus""></span></span>
+                    <h3>Vans</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy  tinc dolore magna.</p>
                 </div>
                 <!-- //Item -->
